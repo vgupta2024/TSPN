3fad4e4fa22f7afcb94b7f2b044e00a5a02b3b7b
varun-gupta24 <varun.gupta24@trinityschoolnyc.org>
2023-04-04 23:44:47 -0400

    n

--- models/sport_model.js
+++ models/sport_model.js
@@ -1,19 +1,5 @@
 const fs = require('fs');
 
-exports.getAllUsers = function(){
-  fs.readFile('users.json',"utf8", function(err, data){
-    let allUsers = JSON.parse(data);
-    return allUsers;
-  })
-}
-
-exports.getAllUsers2 = function(callback) {
-   fs.readFile(__dirname+'/../data/users.json', "utf8", function(err, data){
-      let allUsers = JSON.parse(data);
-      console.log("users2", allUsers)
-      callback(allUsers);
-   });
-}
 
 exports.getAllSports=  function() {
   let allSports = JSON.parse(fs.readFileSync(__dirname+'/../data/sports.json'));
